dd5c7c3c3f682e1ab22439e9ab55b17038ed40ff
maade7 <martin.ade@gatech.edu>
2017-07-17 19:01:20 -0400

    email

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,6 @@
-
-
-
-$(".img-responsive").on("click", function() {
+$(".img-responsive").on("click", function () {
     var vimeoLink = $(this).attr("id");
-    $('#video').attr('src','//player.vimeo.com/video/'+ vimeoLink +'?api=1&player_id=vimeoplayer&title=0&amp;byline=0&amp;portrait=0');
+    $('#video').attr('src', '//player.vimeo.com/video/' + vimeoLink + '?api=1&player_id=vimeoplayer&title=0&amp;byline=0&amp;portrait=0');
 });
 
 var iframe = document.querySelector('iframe');
@@ -16,3 +13,9 @@ $('.modal').on('hidden.bs.modal', function () {
 $('.modal').on('shown.bs.modal', function () {
     player.play();
 });
+
+var frmvalidator = new Validator("contactform");
+frmvalidator.addValidation("name", "req", "Please provide your name");
+frmvalidator.addValidation("email", "req", "Please provide your email");
+frmvalidator.addValidation("email", "email",
+    "Please enter a valid email address");
